02a5a370626b7e9f8aaeb636505014cece86e454
Mark <38960846+auternous@users.noreply.github.com>
2024-02-20 14:41:17 +0300

    iterm2

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -1,13 +1,15 @@
 vim.opt.termguicolors = true
 
 function SetColor(color)
-    color = color or "kanagawa"
-    vim.cmd.colorscheme(color)
+    color = color or "gruvbox"
+    vim.cmd('colorscheme ' .. color)
 
-    -- vim.api.nvim_set_hl(5, "Normal", {bg = "none"})
-    -- vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-    -- vim.api.nvim_set_hl(0, "ColorColumn", {bg = "none"})
-    -- vim.api.nvim_set_hl(0, "LineNr", {bg = "none"})
+    -- Если вы хотите настроить определенные группы подсветки, вы можете сделать это здесь
+    -- Например, чтобы установить фоновый цвет для Normal и NormalFloat на "none":
+    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+    vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+    vim.api.nvim_set_hl(0, "ColorColumn", {bg = "none"})
+    vim.api.nvim_set_hl(0, "LineNr", {bg = "none"})
 end
 
-SetColor(kanagawa)
+SetColor("gruvbox")
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -13,7 +13,8 @@ require("lazy").setup({
         'nvim-telescope/telescope.nvim',
         tag = '0.1.1',
         dependencies = {'nvim-lua/plenary.nvim'}
-    }, {'rebelot/kanagawa.nvim'},
+    }, {"morhetz/gruvbox"},
+
     {'nvim-treesitter/nvim-treesitter'}, {'neovim/nvim-lspconfig'},
     {'hrsh7th/cmp-nvim-lsp'}, {'hrsh7th/cmp-buffer'}, {'hrsh7th/cmp-path'},
     {'hrsh7th/cmp-cmdline'}, {'hrsh7th/nvim-cmp'}, {'lewis6991/gitsigns.nvim'},
