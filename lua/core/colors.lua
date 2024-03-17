069830f5478a5c78e82f0d6d9bb9413c07d32f47
auternous <mark@DESKTOP-MNI9RBR>
2024-03-17 19:52:10 +0300

    opacity

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -1,15 +1,15 @@
 require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    flavour = "frappe", -- latte, frappe, macchiato, mocha
     background = { -- :h background
         light = "latte",
         dark = "mocha",
     },
-    transparent_background = false, -- disables setting the background color.
+    transparent_background = true, -- disables setting the background color.
     show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
     term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
     dim_inactive = {
         enabled = false, -- dims the background color of inactive window
-        shade = "dark",
+        shade = "light",
         percentage = 0.15, -- percentage of the shade to apply to the inactive window
     },
     no_italic = false, -- Force no italic
